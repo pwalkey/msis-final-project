de8f4cf5da012800bdf8d7b606ecd2dfb4e6b00e
bsstu <70525019+bsstu@users.noreply.github.com>
2020-10-16 01:00:29 -0400

    update php

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -2,20 +2,54 @@ var app = new Vue({
   el: '#phppage',
 
   data: {
-  	personList: []
+	  personList: [],
+	  newmemberForm: {}
   },
 
-//   methods: {
-created(){
-  	// console.log("6");
-  	fetch("php/Person/")
-  	.then(response => response.json())
-  	.then( json => {
-  		this.personList = json;
-  		console.log(json)
-  	});
-}
-//   }
-
+  methods: {
+	newmemberData() {
+		return {
+			PersonID: "",
+			LastName: "",
+			FirstName: "",
+			RadioNumber: "",
+			StationNumber: "",
+			Position: ""
+		}
+	  },
+	handleNewMemberForm( evt ) {
+		// evt.preventDefault();  // Redundant w/ Vue's submit.prevent
+  
+		// TODO: Validate the data!
+  
+		fetch('php/Person/post.php', {
+		  method:'POST',
+		  body: JSON.stringify(this.newmemberForm),
+		  headers: {
+			"Content-Type": "application/json; charset=utf-8"
+		  }
+		})
+		.then( response => response.json() )
+		.then( json => {
+		  console.log("Returned from post:", json);
+		  // TODO: test a result was returned!
+		  this.psList.push(json[0]);
+		  this.newmemberForm = this.newmemberData();
+		});
+  
+		console.log("Creating (POSTing)...!");
+		console.log(this.newmemberForm);
+	  },
+	},
+	created(){
+		// console.log("6");
+		fetch("php/Person/")
+		.then(response => response.json())
+		.then( json => {
+			this.personList = json;
+			console.log(this.personList)
+		});
+		this.newmemberForm = this.newmemberData();
+	}
 })
 
--- app/public/php/Person/index.php
+++ app/public/php/Person/index.php
@@ -9,6 +9,12 @@ $db = DbConnection::getConnection();
 $sql = 'SELECT * FROM PERSON';
 $vars = [];
 
+if (isset($_GET['pk'])) {
+    // This is an example of a parameterized query
+    $sql = 'SELECT * FROM Person WHERE PersonID = ?';
+    $vars = [ $_GET['pk'] ];
+  }
+
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
