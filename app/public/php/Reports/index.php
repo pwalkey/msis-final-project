26422df2a50608783a82d805852f1a372e99a058
nstj <70527768+nstj@users.noreply.github.com>
2020-11-02 22:52:36 -0500

    Renamed to index.php to match other folders. Fixed query string using " instead of '

---
+++ app/public/php/Reports/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = "SELECT * FROM PERSONCERTIFICATION PC, PERSON P, CERTIFICATION C WHERE 
+   (YEAR(CURDATE()) - YEAR(PC.DateAcquired) - (DATE_FORMAT(CURDATE(), '%m%d') 
+   < DATE_FORMAT(PC.DateAcquired, '%m%d'))) > 
+   C.ExpirationPeriod AND P.PersonID = PC.PersonID 
+   AND C.CertificationID = PC.CertificationID;";
+
+$vars = [];
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$user = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($user, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
