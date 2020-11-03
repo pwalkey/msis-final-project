f0aa9814468471061dce98d6fe9a3c9754ffa4df
mattevans2 <70525313+mattevans2@users.noreply.github.com>
2020-11-02 22:42:25 -0500

    Add files via upload
    Person: delete and update php final commit

---
+++ app/public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM PERSON';
+$vars = [];
+
+if (isset($_GET['PersonID'])) {
+    // This is an example of a parameterized query
+    $sql = 'SELECT * FROM Person WHERE PersonID = ?';
+    $vars = [ $_GET['PersonID'] ];
+  }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$person = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($person, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
